e4e2333af319b990c6e9d40b985ee4537f8ad37b
HungryChaika <vologzhanin_00@mail.ru>
2023-08-26 11:45:12 +0400

    Cosmetic changes #2

--- app/src/main/java/com/example/flashlightxml/domain/usecase/SOSUseCase.kt
+++ app/src/main/java/com/example/flashlightxml/domain/usecase/SOSUseCase.kt
@@ -1,15 +1,13 @@
 package com.example.flashlightxml.domain.usecase
 
-
 import com.example.flashlightxml.domain.repository.FlashLightRepository
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-// Класс для сигнала SOS
 class SOSUseCase(private val repository: FlashLightRepository) {
 
-    private val camManager= repository.get().camManager
+    private val camManager = repository.get().camManager
 
     companion object {
         private const val COUNT_OF_FLASHES = 9
@@ -21,13 +19,13 @@ class SOSUseCase(private val repository: FlashLightRepository) {
     private suspend fun sosSignal() {
         var counter = 1
         var isLongFlash = false
-        while(counter <= COUNT_OF_FLASHES) {
-            when(isLongFlash) {
+        while (counter <= COUNT_OF_FLASHES) {
+            when (isLongFlash) {
                 true -> flash(LONG)
                 false -> flash(SHORT)
             }
             delay(WAITING)
-            if(counter % 3 == 0) isLongFlash = !isLongFlash
+            if (counter % 3 == 0) isLongFlash = !isLongFlash
             counter++
         }
     }
